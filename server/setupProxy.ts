73a3e5b67c7cdc867464a0b8e5b93b95478ea042
Andy Ficek <aficek@precisionplanting.com>
2023-10-06 10:25:35 -0500

    trying to get this working

--- server/setupProxy.ts
+++ server/setupProxy.ts
@@ -1,38 +1,44 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-const app = express();
-const port = process.env.PORT || 3000;
 
-app.use(cors());
-const response = await fetch(`https://carapi.app/api/auth/login`, {
-  method: "POST",
-  headers: {
-    Accept: "text/plain",
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    api_token: process.env.CARAPI_TOKEN,
-    api_secret: process.env.CARAPI_SECRET,
-  }),
-});
+const setupServer = async () => {
+  const app = express();
+  const port = process.env.PORT || 3000;
+  app.use(cors());
 
-const data = await response.text();
-console.log("before", data);
-app.use((req, res) => {
-  fetch(`https://carapi.app/api${req.path}`, {
+  const response = await fetch(`https://carapi.app/api/auth/login`, {
+    method: "POST",
     headers: {
-      Accept: "application/json",
+      Accept: "text/plain",
       "Content-Type": "application/json",
-      Authorization: `Bearer ${data}`,
     },
-  })
-    .then((response) => response.json())
-    .then((data) => res.json(data))
-    .catch((error) => console.error("Error:", error));
-});
-console.log("after", data);
+    body: JSON.stringify({
+      api_token: process.env.CARAPI_TOKEN,
+      api_secret: process.env.CARAPI_SECRET,
+    }),
+  });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+  const data = await response.text();
+  console.log("before", data);
+
+  app.use((req, res) => {
+    fetch(`https://carapi.app/api${req.path}`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${data}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => res.json(data))
+      .catch((error) => console.error("Error:", error));
+  });
+  console.log("after", data);
+
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
+};
+
+setupServer();
